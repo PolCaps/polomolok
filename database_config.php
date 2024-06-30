7788a522876e8a18f99f99a9919a50f4a13b8033
PolCaps <polcapstone9504@gmail.com>
2024-06-30 18:02:39 +0800

    database configuration

--- database_config.php
+++ database_config.php
@@ -1,6 +1,14 @@
 <?php
+
 $db_host = 'localhost';
 $db_user = 'root';
 $db_password = '';
-$db_name = 'PolomolokPublicMarket';
+$db_name = 'polomolokpublicmarket';
+
+// $db_host = 'polomolokpublicmarket';
+// $db_user = 'nvhsudih_admin';
+// $db_password = 'O?7i$*)nIX;~';
+// $db_name = 'nvhsudih_polomolokpublicmarket';
+
 ?>
+
