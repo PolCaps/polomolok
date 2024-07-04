e8ebafd3fe6c22b8f3f26b698c98de607e3c1b20
PolCaps <polcapstone9504@gmail.com>
2024-07-04 19:06:24 +0800

    july4

---
+++ database_config.php
@@ -0,0 +1,22 @@
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'polomolokpublicmarket';
+
+
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+                      
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+// $db_host = 'polomolokpublicmarket';
+// $db_user = 'nvhsudih_admin';
+// $db_password = 'O?7i$*)nIX;~';
+// $db_name = 'nvhsudih_polomolokpublicmarket';
+
+?>
+
