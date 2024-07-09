1f64efd4dbc32296f556d4fbcdeefbfb856a9f5e
PolCaps <polcapstone9504@gmail.com>
2024-07-09 09:17:47 +0800

    polomolokDATABASE

--- database/polomolokpublicmarket.sql
+++ database/polomolokpublicmarket.sql
@@ -3,11 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2024 at 05:55 PM
+-- Generation Time: Jul 09, 2024 at 03:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -22,78 +24,329 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users_account`
+-- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `users_account`;
-CREATE TABLE `users_account` (
-  `account_id` int(11) NOT NULL,
-  `user_type` varchar(250) NOT NULL,
-  `username` varchar(250) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `first_name` varchar(250) NOT NULL,
-  `middle_name` varchar(250) NOT NULL,
-  `last_name` varchar(250) NOT NULL,
-  `age` int(50) NOT NULL,
-  `contact_number` bigint(100) NOT NULL,
-  `address` varchar(500) NOT NULL,
-  `store_number` bigint(150) NOT NULL,
-  `building_type` varchar(200) NOT NULL,
-  `started_date` varchar(100) NOT NULL,
-  `end_date` varchar(100) NOT NULL,
-  `monthly_payment` bigint(250) NOT NULL,
-  `lease_agreements` varchar(60) DEFAULT NULL,
-  `business_permits` varchar(60) DEFAULT NULL,
-  `supporting_docu` varchar(60) DEFAULT NULL
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buildings`
+--
+
+CREATE TABLE `buildings` (
+  `building_id` int(11) NOT NULL,
+  `building_floor` varchar(255) NOT NULL,
+  `building_type` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `vendor_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- RELATIONSHIPS FOR TABLE `users_account`:
+-- Dumping data for table `buildings`
 --
 
+INSERT INTO `buildings` (`building_id`, `building_floor`, `building_type`, `user_id`, `vendor_id`) VALUES
+(37, 'Building J', 'Ground Floor', 137, NULL),
+(38, 'Building J', 'Ground Floor', 137, NULL);
+
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `users_account`
+-- Table structure for table `documents`
 --
 
-INSERT INTO `users_account` (`account_id`, `user_type`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `age`, `contact_number`, `address`, `store_number`, `building_type`, `started_date`, `end_date`, `monthly_payment`, `lease_agreements`, `business_permits`, `supporting_docu`) VALUES
-(1, 'Admin', 'meedo', 'meedo123', '', '', '', 0, 0, '', 0, '', '2024-06-26', '2024-06-26', 0, '', '', ''),
-(2, 'Vendor', 'vendors', 'vendor123', '', '', '', 0, 0, '', 0, '', '2024-06-26', '2024-06-26', 0, '', '', ''),
-(3, 'Vendor', 'MarkMeedo', 'mark123', 'REYAN JAN', 'BARRANCO', 'SAMONTANES', 22, 9510462062, 'Polotana Subdivision, West Drive.', 2424, 'residential', '2024-06-27', '2024-07-31', 12333, 'table.txt', 'table.txt', 'table.txt'),
-(4, 'Staff', 'Prynce', 'prince1234', 'Prynce Jay', 'Omar', 'Sayre', 23, 9510462062, 'bawing', 2010, 'commercial', '2024-06-27', '2024-07-31', 1200, 'table.txt', 'PlantsVsZombies.exe', 'PlantsVsZombies.exe');
+CREATE TABLE `documents` (
+  `document_id` int(11) NOT NULL,
+  `receipts` longblob DEFAULT NULL,
+  `lease_agreements` longblob DEFAULT NULL,
+  `business_permits` longblob DEFAULT NULL,
+  `business_licenses` longblob DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`document_id`, `receipts`, `lease_agreements`, `business_permits`, `business_licenses`, `user_id`) VALUES
+(18, 0x64617461732f6d6565646f2f323032342d30372d30382f327031306e657433302e706e67, 0x64617461732f6d6565646f2f323032342d30372d30382f327031306e657433302e706e67, 0x64617461732f6d6565646f2f323032342d30372d30382f3320496e7374616c6c6d656e74732e706e67, 0x64617461732f6d6565646f2f323032342d30372d30382f3320496e7374616c6c6d656e74732e706e67, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monthly_payments`
+--
+
+CREATE TABLE `monthly_payments` (
+  `payment_id` int(11) NOT NULL,
+  `SOA` longblob DEFAULT NULL,
+  `receipts` longblob DEFAULT NULL,
+  `started_date` varchar(255) DEFAULT NULL,
+  `end_date` varchar(255) DEFAULT NULL,
+  `document_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `monthly_payments`
+--
+
+INSERT INTO `monthly_payments` (`payment_id`, `SOA`, `receipts`, `started_date`, `end_date`, `document_id`, `user_id`) VALUES
+(6, NULL, NULL, '2024-07-25', '2024-08-22', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_profile`
+--
+
+CREATE TABLE `personal_profile` (
+  `personal_id` int(11) DEFAULT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `contact_number` bigint(20) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `personal_profile`
+--
+
+INSERT INTO `personal_profile` (`personal_id`, `first_name`, `middle_name`, `last_name`, `age`, `contact_number`, `address`) VALUES
+(137, 'REYAN JAN', 'BARRANCO', 'SAMONTANES', 22, 9510462062, '9504\r\nWEST DRIVE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staffs`
+--
+
+CREATE TABLE `staffs` (
+  `staffs_id` int(11) NOT NULL,
+  `staffs_name` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stalls`
+--
+
+CREATE TABLE `stalls` (
+  `stall_code` varchar(255) NOT NULL,
+  `monthly_rentals` varchar(255) NOT NULL,
+  `stall_number` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `building_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stalls`
+--
+
+INSERT INTO `stalls` (`stall_code`, `monthly_rentals`, `stall_number`, `user_id`, `building_id`) VALUES
+('BJ12', '12312', '12', 137, 38);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(100) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` enum('ADMIN','VENDOR','STAFF') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `user_type`) VALUES
+(101, 'meedo', 'meedo123', 'ADMIN'),
+(137, 'meedo', 'meedo123', 'VENDOR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendors`
+--
+
+CREATE TABLE `vendors` (
+  `vendor_id` int(11) NOT NULL,
+  `vendor_name` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vendors`
+--
+
+INSERT INTO `vendors` (`vendor_id`, `vendor_name`, `user_id`) VALUES
+(0, 'REYAN JAN BARRANCO SAMONTANES', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users_account`
+-- Indexes for table `admin`
 --
-ALTER TABLE `users_account`
-  ADD PRIMARY KEY (`account_id`);
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `buildings`
+--
+ALTER TABLE `buildings`
+  ADD PRIMARY KEY (`building_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `vendor_id` (`vendor_id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`document_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `monthly_payments`
+--
+ALTER TABLE `monthly_payments`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `fk_document_id` (`document_id`);
+
+--
+-- Indexes for table `personal_profile`
+--
+ALTER TABLE `personal_profile`
+  ADD KEY `personal_id` (`personal_id`);
+
+--
+-- Indexes for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD PRIMARY KEY (`staffs_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `stalls`
+--
+ALTER TABLE `stalls`
+  ADD PRIMARY KEY (`stall_code`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `building_id` (`building_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `vendors`
+--
+ALTER TABLE `vendors`
+  ADD PRIMARY KEY (`vendor_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users_account`
+-- AUTO_INCREMENT for table `buildings`
 --
-ALTER TABLE `users_account`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
-
---
--- Metadata
---
-USE `phpmyadmin`;
+ALTER TABLE `buildings`
+  MODIFY `building_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
--- Metadata for table users_account
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `monthly_payments`
+--
+ALTER TABLE `monthly_payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Metadata for database polomolokpublicmarket
+-- Constraints for table `admin`
 --
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `buildings`
+--
+ALTER TABLE `buildings`
+  ADD CONSTRAINT `buildings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `buildings_ibfk_2` FOREIGN KEY (`vendor_id`) REFERENCES `vendors` (`vendor_id`);
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `monthly_payments`
+--
+ALTER TABLE `monthly_payments`
+  ADD CONSTRAINT `fk_document_id` FOREIGN KEY (`document_id`) REFERENCES `documents` (`document_id`),
+  ADD CONSTRAINT `monthly_payments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `monthly_payments_ibfk_2` FOREIGN KEY (`document_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `personal_profile`
+--
+ALTER TABLE `personal_profile`
+  ADD CONSTRAINT `personal_profile_ibfk_1` FOREIGN KEY (`personal_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD CONSTRAINT `staffs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `stalls`
+--
+ALTER TABLE `stalls`
+  ADD CONSTRAINT `stalls_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `stalls_ibfk_2` FOREIGN KEY (`building_id`) REFERENCES `buildings` (`building_id`);
+
+--
+-- Constraints for table `vendors`
+--
+ALTER TABLE `vendors`
+  ADD CONSTRAINT `vendors_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
