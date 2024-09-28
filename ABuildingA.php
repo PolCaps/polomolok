3ae0095198e21d9b1b7f4aaeb695cae19010c613
PolCaps <polcapstone9504@gmail.com>
2024-09-28 18:33:30 +0800

    implemented color coded sa map

--- ABuildingA.php
+++ ABuildingA.php
@@ -102,12 +102,16 @@ $user_id = $_SESSION['id'];
 
 
 <body class="g-sidenav-show  bg-gray-100">
-  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 "
+    id="sidenav-main">
     <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+        aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="#" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-list" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-list"
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
         </svg>
         <span class="ms-2 font-weight-bold">Menu</span>
       </a>
@@ -117,32 +121,39 @@ $user_id = $_SESSION['id'];
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link " href="Admin.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-shop" viewBox="0 0 16 16">
-                <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-shop"
+                viewBox="0 0 16 16">
+                <path
+                  d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z" />
               </svg>
             </div>
             <span class="nav-link-text ms-1">Dashboard</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseMaps" aria-expanded="false" aria-controls="collapseMaps">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-map-fill" viewBox="0 0 16 16">
+          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseMaps"
+            aria-expanded="false" aria-controls="collapseMaps">
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-pin-map-fill" viewBox="0 0 16 16">
                 <title>office</title>
-                    <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z"/>
-                    <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"/>
+                <path fill-rule="evenodd"
+                  d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z" />
+                <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z" />
               </svg>
             </div>
             <span class="nav-link-text ms-1">Maps</span>
           </a>
           <div class="collapse" id="collapseMaps">
             <div class="right-aligned-links" style="text-align: right;">
-              <a class="nav-link active" href="ABuildingA.php">Building A</a>
-              <a class="nav-link" href="ABuildingB.php">Building B</a>
-              <a class="nav-link" href="ABuildingC.php">Building C</a>
-              <a class="nav-link" href="ABuildingD.php">Building D</a>
-              <a class="nav-link" href="ABuildingE.php">Building E</a>
+              <a class="nav-link" href="ABuildingA.php">Building A</a>
+              <a class="nav-link" href="ABuildingB.html">Building B</a>
+              <a class="nav-link" href="ABuildingC.html">Building C</a>
+              <a class="nav-link" href="ABuildingD.html">Building D</a>
+              <a class="nav-link" href="ABuildingE.html">Building E</a>
               <a class="nav-link" href="ABuildingF.html">Building F</a>
               <a class="nav-link" href="ABuildingG.html">Building G</a>
               <a class="nav-link" href="ABuildingH.html">Building H</a>
@@ -152,62 +163,96 @@ $user_id = $_SESSION['id'];
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="AMvendor.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
-                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/>
+          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAccounts"
+            aria-expanded="false" aria-controls="collapseAccounts">
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000"
+                class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                <path
+                  d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Vendors/Users</span>
-          </a> 
+            <span class="nav-link-text ms-1">Accounts</span>
+          </a>
+          <div class="collapse" id="collapseAccounts">
+            <div class="right-aligned-links" style="text-align: right;">
+              <a class="nav-link" href="AMUser.php">Users</a>
+              <a class="nav-link" href="AMVendor.php">Vendors</a>
+            </div>
+          </div>
         </li>
         <li class="nav-item">
           <a class="nav-link " href="AMmessages.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
-            </svg>
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                <path
+                  d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+              </svg>
             </div>
             <span class="nav-link-text ms-1">Messages</span>
-          </a> 
+          </a>
         </li>
         <li class="nav-item">
           <a class="nav-link " href="AMinquiries.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-dots-fill" viewBox="0 0 16 16">
-              <path d="M16 8c0 3.866-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7M5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-            </svg>
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                class="bi bi-chat-dots-fill" viewBox="0 0 16 16">
+                <path
+                  d="M16 8c0 3.866-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7M5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+              </svg>
             </div>
             <span class="nav-link-text ms-1">Inquiries</span>
-          </a> 
+          </a>
         </li>
 
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Vendor and Stall Approval</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="AMVendorApproval.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-            <img src="image/icons/icons8-approve-48.png" alt="approveicon" width="18px" height="18px">
+          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseVendorApproval"
+            aria-expanded="false" aria-controls="collapseVendorApproval">
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <img src="image/icons/icons8-approve-48.png" alt="approveicon" width="18px" height="18px">
             </div>
             <span class="nav-link-text ms-1">Vendor Approval</span>
           </a>
+          <div class="collapse" id="collapseVendorApproval">
+            <div class="right-aligned-links" style="text-align: right;">
+              <a class="nav-link" href="AMStallApp.php">Stall Applicants</a>
+              <a class="nav-link" href="AMReadydraw.php">Ready for Drawlots</a>
+            </div>
+          </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="AMRelocationRequest.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseRelRequest"
+            aria-expanded="false" aria-controls="collapseRelRequest">
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <img src="image/icons/icons8-kiosk-on-wheels-48.png" alt="relocationimg" width="18px" height="18px">
             </div>
             <span class="nav-link-text ms-1">Relocation Request</span>
           </a>
+          <div class="collapse" id="collapseRelRequest">
+            <div class="right-aligned-links" style="text-align: right;">
+              <a class="nav-link" href="AMRelReqApprove.php">Approved</a>
+              <a class="nav-link" href="AMRelReqProcessing.php">Processing</a>
+              <a class="nav-link" href="AMRelReqDeclined.php">Declined</a>
+            </div>
+          </div>
         </li>
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Page Customization</h6>
         </li>
         <li class="nav-item">
           <a class="nav-link  " href="HomepageEditor.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-            <img src="image/icons/icons8-browser-settings-48.png" alt="approveicon" width="18px" height="18px">
+            <div
+              class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <img src="image/icons/icons8-browser-settings-48.png" alt="approveicon" width="18px" height="18px">
             </div>
             <span class="nav-link-text ms-1">Homepage</span>
           </a>
@@ -218,12 +263,20 @@ $user_id = $_SESSION['id'];
       <div class="card card-background shadow-none card-background-mask-info" id="sidenavCard">
         <div class="full-background" style="background-image: url('assets2/img/curved-images/white-curved.jpg')"></div>
         <div class="card-body text-start p-3 w-100">
-          <img src="image/profile.jpg" alt="profile" style="min-width: 20px; min-height: 20px; height: 100px; width: 100px; border-radius: 10px; margin-left: 40px;">
-          <h5 class="text-center"><?php echo $user['username'];?></h5>
+          <?php
+          // Check if the user has a profile picture set; if not, use a default image
+          $profilePicture = !empty($user['picture_profile']) ? $user['picture_profile'] : 'image/profile.jpg';
+          ?>
+          <img src="<?php echo htmlspecialchars($profilePicture); ?>" class="img-fluid" alt="Admin Profile Picture"
+            style="min-width: 20px; min-height: 20px; height: 100px; width: 100px; margin-left: 40px;">
+          <h5 class="text-center mt-2">
+            <?php echo htmlspecialchars($user['first_name']) . ' ' . htmlspecialchars($user['middle_name']) . ' ' . htmlspecialchars($user['last_name']); ?>
+          </h5>
           <hr class="horizontal dark mt-0">
         </div>
       </div>
     </div>
+
   </aside>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
@@ -307,7 +360,7 @@ $user_id = $_SESSION['id'];
                         if ($result->num_rows > 0) {
                             // Fetch all occupied stalls
                             while($row = $result->fetch_assoc()) {
-                                $occupied_stalls[] = $row['stall_number'];
+                                $occupied_stalls[] = $row['stall_no'];
                             }
                         } else {
                             $occupied_stalls[] = "No occupied stalls found";
@@ -9244,7 +9297,6 @@ $user_id = $_SESSION['id'];
  <g id="lcg">
   <path id="lch" d="m812.39 1123 3.0656 38.831 41.897 49.05 4.0875-91.969z" style="opacity:0;stroke-width:6"/>
  </g>
-
  
  <a id="A-68" data-stall-no="A-68" data-bs-toggle="tooltip" type="button" class="btn btn-secondary"
     data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="">
@@ -9379,7 +9431,7 @@ $user_id = $_SESSION['id'];
  
 
   
- <a id="51" data-stall-no="51" data-bs-toggle="tooltip" type="button" class="btn btn-secondary"
+ <a id="51" data-stall-no="A-51" data-bs-toggle="tooltip" type="button" class="btn btn-secondary"
     data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="">
     <g id="ldo">
   <path id="ldp" d="m181.89 195.18 3.0656 113.43 72.553-3.0656-1.0219-107.3z" style="opacity:0;stroke-width:6"/>
@@ -9790,6 +9842,21 @@ $user_id = $_SESSION['id'];
         .then(data => {
           // Update the title attribute with both the stall status and occupant
           tooltipTriggerEl.setAttribute('title', 'Status: ' + data.stall_status + ' | Occupant: ' + data.vendor_name);
+
+          // Dynamically set the tooltip background color based on stall status
+          var tooltipClass = 'custom-tooltip-' + stallNo; // Unique class for each tooltip
+          
+          var tooltipStyles = document.createElement('style');
+          tooltipStyles.innerHTML = `
+            .${tooltipClass} .tooltip-inner {
+              background-color: ${data.stall_status === 'Vacant' ? 'green' : 'red'} !important;
+            }
+          `;
+          document.head.appendChild(tooltipStyles);
+
+          // Add the custom class to the tooltip
+          tooltipTriggerEl.setAttribute('data-bs-custom-class', tooltipClass);
+
           // Initialize the tooltip
           new bootstrap.Tooltip(tooltipTriggerEl);
         })
