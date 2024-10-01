16994658b2dd43b51c3787a67f34ebc718b90aa1
PolCaps <polcapstone9504@gmail.com>
2024-10-01 10:54:08 +0800

    cashier small renaming

--- CMReceiptApplicants.php
+++ CMReceiptApplicants.php
@@ -49,7 +49,7 @@ if (!$user) {
   <link rel="apple-touch-icon" sizes="76x76" href="assets2/img/apple-icon.png">
   <link rel="icon" type="image/png" href="assets/imgbg/BGImage.png">
   <title>
-    Dashboard
+    Cashier
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
